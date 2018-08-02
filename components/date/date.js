8963883c464904ff13e9a68d5f0ade0caf246ecf
dongyang <843408503@qq.com>
2018-08-02 20:28:27 +0800

    格式化

--- components/date/date.js
+++ components/date/date.js
@@ -1,250 +1,307 @@
+function newDate(el){
+  const box = document.getElementById(el);
+  const template = ['<div class="date-components" id="date-components">',
+'        <div class="express-day">',
+'            <div id = "starDay"></div>',
+'            <div id = "endDay"></div>',
+'        </div>',
+'        <div>',
+'            <div id = "cpdate-selectStar">',
+'              <div class="title">',
+'                  <div id="cpdate-preYear"> << </div>',
+'                  <div id="cpdate-preMon"> < </div>',
+'                  <div id="cpdate-month"></div>',
+'                  <div id="cpdate-nextMon"> > </div>',
+'                  <div id="cpdate-nextYear"> >> </div>',
+'                </div>',
+'                <table class="date-table" id ="cpdate-table" cellpadding = 0; cellspacing=0; rules="all"> ',
+'                </table>',
+'            </div>',
+'            <div id = "cpdate-selectEnd">',
+'                <div class="title">',
+'                  <div id="cpdate-preYear2"> << </div>',
+'                  <div id="cpdate-preMon2"> < </div>',
+'                  <div id="cpdate-month2"></div>',
+'                  <div id="cpdate-nextMon2"> > </div>',
+'                  <div id="cpdate-nextYear2"> >> </div>',
+'                </div>',
+'                <table  class="date-table" id ="cpdate-table2" cellpadding = 0; cellspacing=0; rules="all"> ',
+'                </table>',
+'            </div>',
+'        </div>',
+'        <div id = "confirmDate">确定</div>',
+'  </div>'].join("");
 
-let starDay = new Date();
-let endDay = new Date();
+  box.innerHTML = template;
+  
+  let starDay = new Date();
+  let endDay = new Date();
 
-let type1 = {
-    curTrArr :[],
-    preMon:"cpdate-preMon",
-    nextMon:"cpdate-nextMon",
-    preYear:"cpdate-preYear",
-    nextYear:"cpdate-nextYear",
-    objTable :"cpdate-table",
-    objTitle:"cpdate-month"
-}
-
-let type2 = {
-    curTrArr :[],
-    preMon:"cpdate-preMon2",
-    nextMon:"cpdate-nextMon2",
-    preYear:"cpdate-preYear2",
-    nextYear:"cpdate-nextYear2",
-    objTable :"cpdate-table2",
-    objTitle:"cpdate-month2"
-}
-
-let date = {
-  _year : starDay.getFullYear(),
-  _month : starDay.getMonth()+1,
-  _day : starDay.getDate(),
-}
-
-Object.defineProperty(date, 'year', {
-  set: function(newValue){
-     this._year = newValue;
-     showTitle(date,"starDay");
-  },
-  get: function() {
-     return this._year;
+  let type1 = {
+      curTrArr :[],
+      preMon:"cpdate-preMon",
+      nextMon:"cpdate-nextMon",
+      preYear:"cpdate-preYear",
+      nextYear:"cpdate-nextYear",
+      objTable :"cpdate-table",
+      objTitle:"cpdate-month"
   }
-});
 
-Object.defineProperty(date, 'month', {
-  set: function(newValue){
-     this._month = newValue;
-     showTitle(date,"starDay");
-  },
-  get: function() {
-     return this._month;
+  let type2 = {
+      curTrArr :[],
+      preMon:"cpdate-preMon2",
+      nextMon:"cpdate-nextMon2",
+      preYear:"cpdate-preYear2",
+      nextYear:"cpdate-nextYear2",
+      objTable :"cpdate-table2",
+      objTitle:"cpdate-month2"
   }
-});
-
-Object.defineProperty(date, 'day', {
-  set: function(newValue){
-     this._day = newValue;
-     showTitle(date,"starDay");
-  },
-  get: function() {
-     return this._day;
+  
+  let date = {
+    _year : starDay.getFullYear(),
+    _month : starDay.getMonth()+1,
+    _day : starDay.getDate(),
   }
-});
 
-
-let date2 = {
-  _year : endDay.getFullYear(),
-  _month : endDay.getMonth()+1,
-  _day : endDay.getDate(),
-}
-
-Object.defineProperty(date2, 'year', {
-  set: function(newValue){
-     this._year = newValue;
-     showTitle(date2,"endDay");
-  },
-  get: function() {
-     return this._year;
+  this.getDate = function(){
+      return function(date,date2){
+          reuturn (date,date2)
+      }
   }
-});
 
-Object.defineProperty(date2, 'month', {
-  set: function(newValue){
-     this._month = newValue;
-     showTitle(date2,"endDay");
-  },
-  get: function() {
-     return this._month;
-  }
-});
-
-Object.defineProperty(date2, 'day', {
-  set: function(newValue){
-     this._day = newValue;
-     showTitle(date2,"endDay");
-  },
-  get: function() {
-     return this._day;
-  }
-});
-
-
-init(date,date2,type1,type2);
-showTitle(date,"starDay");
-showTitle(date2,"endDay");
-
-
-
-//初始化
-function init(date,date2,type1,type2){
-  show(date,type1);
-  add( date,type1);
-  show(date2,type2);
-  add( date2,type2);
-}
-
-
-//标题的渲染函数
-function showTitle(date,obj){
-  let objDate = document.getElementById(obj);
-  objDate.innerHTML = date.year +"年"+ date.month + "月" + date.day + "日";
-}
-
-function add(date,type){
-  addPreMon(date,type);
-  addNextMon(date,type);
-  addPreYear(date,type);
-  addNextYear(date,type);
-  addSelect(date,type);
-}
-
-//添加前一个月点击事件事件
-function addPreMon(date,type){
-  let preMon = document.getElementById(type.preMon);
-  preMon.addEventListener("click", function(e){
-    if( date.month > 1){
-       date.month--;
-    }else{
-      date.year--;
-      date.month = 12;
+  Object.defineProperty(date, 'year', {
+    set: function(newValue){
+       this._year = newValue;
+       showTitle(date,"starDay");
+    },
+    get: function() {
+       return this._year;
     }
-    show(date,type);
-  })
-}
+  });
 
-//添加下一个月点击事件
-function addNextMon(date,type){
-  let nextMon = document.getElementById(type.nextMon);
-  nextMon.addEventListener("click", function(e){
-      if( date.month<12){
-         date.month++;
+  Object.defineProperty(date, 'month', {
+    set: function(newValue){
+       this._month = newValue;
+       showTitle(date,"starDay");
+    },
+    get: function() {
+       return this._month;
+    }
+  });
+
+  Object.defineProperty(date, 'day', {
+    set: function(newValue){
+       this._day = newValue;
+       showTitle(date,"starDay");
+    },
+    get: function() {
+       return this._day;
+    }
+  });
+
+
+  let date2 = {
+    _year : endDay.getFullYear(),
+    _month : endDay.getMonth()+1,
+    _day : endDay.getDate(),
+  }
+
+  Object.defineProperty(date2, 'year', {
+    set: function(newValue){
+       this._year = newValue;
+       showTitle(date2,"endDay");
+    },
+    get: function() {
+       return this._year;
+    }
+  });
+
+  Object.defineProperty(date2, 'month', {
+    set: function(newValue){
+       this._month = newValue;
+       showTitle(date2,"endDay");
+    },
+    get: function() {
+       return this._month;
+    }
+  });
+
+  Object.defineProperty(date2, 'day', {
+    set: function(newValue){
+       this._day = newValue;
+       showTitle(date2,"endDay");
+    },
+    get: function() {
+       return this._day;
+    }
+  });
+
+  init(date,date2,type1,type2);
+  showTitle(date,"starDay");
+  showTitle(date2,"endDay");
+
+  //初始化
+  function init(date,date2,type1,type2){
+    show(date,type1);
+    add( date,type1);
+    show(date2,type2);
+    add( date2,type2);
+  }
+  
+  //定义拿到date值的方法
+  this.getDate = function(){
+    return {date,date2}
+  }
+
+
+  //标题的渲染函数
+  function showTitle(date,obj){
+    let objDate = document.getElementById(obj);
+    objDate.innerHTML = date.year +"年"+ date.month + "月" + date.day + "日";
+  }
+
+  function add(date,type){
+    addPreMon(date,type);
+    addNextMon(date,type);
+    addPreYear(date,type);
+    addNextYear(date,type);
+    addSelect(date,type);
+  }
+
+  //添加前一个月点击事件事件
+  function addPreMon(date,type){
+    let preMon = document.getElementById(type.preMon);
+    preMon.addEventListener("click", function(e){
+      if( date.month > 1){
+         date.month--;
       }else{
-        date.year++;
-        date.month = 1;
+        date.year--;
+        date.month = 12;
       }
-     show(date,type);
-  })
-}
-
-//添加前一年点击事件
-function addPreYear(date,type){
-  let preYear = document.getElementById(type.preYear);
-  preYear.addEventListener("click", function(e){
-    date.year--
-    show(date,type);
-  })
-}
-
-//添加前一年点击事件
-function addNextYear(date,type){
-  let nextYear = document.getElementById(type.nextYear);
-  nextYear.addEventListener("click", function(e){
-      date.year++
       show(date,type);
-  })
-}
-
-//添加选择日期事件
-function addSelect(date,type){
-  let objTable = document.getElementById(type.objTable);
-  objTable.addEventListener("click",function(e){
-   if(e.target.nodeName === "TD" && e.target.innerHTML !==""){
-     date.day = Number(e.target.innerHTML);
-     currentShow(type,date.day)
-    }
-  })
-}
-
-function show(date,type){
-  let year = date.year;
-  let month = date.month;
-  let day = date.day;
-  let objMonth = document.getElementById(type.objTitle);
-  objMonth.innerHTML =year + "年" + month + "月";
-  type.curTrArr = creatDate(date);
-  currentShow(type,day);
-}
-
-
-function currentShow(type,day){
-   let html = renderDate(type.curTrArr,day,type);
-   let objTable = document.getElementById(type.objTable);
-   objTable.innerHTML = html;
-}
-
-
-
- 
-
-// ********************纯函数************************
-
-//创建日期的二维数组;
-function creatDate(date){
-  let year = date.year;
-  let month = date.month;
-  let day = date.day;
-  let str = year + "-"+ month + "-" + "1";
-  let week = new Date(str).getDay();
-  let dayCountObj = new Date(year,month,0);
-  let dayCount = dayCountObj.getDate();
-  let j = 1;
-  let trArr = [];
-  for (let a = 0; a < 6; a++){
-    trArr[a] = [];
-    for (let i = 0; i < 7; i++){
-      if( (i >= week || a > 0) && j<= dayCount){
-           trArr[a][i] = j++;
-       }else {
-           trArr[a][i] = "";
-      }
-    }
-  } 
-  return trArr;
-}
-
-//渲染成HTML模版
-function renderDate(trArr,day,type){
-  let html = "<tr><th>日</th><th>一</th><th>二</th><th>三</th><th>四</th><th>五</th><th>六</th></tr>";
-  for (let i = 0; i < 6; i++){
-   if(i<5||(i == 5 && trArr[i][0]!=='')){
-      let template = "";
-      for(let n = 0; n < 7; n++){
-         if(trArr[i][n] == day){
-           template += "<td  class='date-select'>"+trArr[i][n]+"</td>"
-         }else {
-           template += "<td>"+trArr[i][n]+"</td>"
-         }
-      }
-      templateBox = "<tr>"+template+"</tr>";
-      html += templateBox;
-   } 
+    })
   }
-  return html;
+
+  //添加下一个月点击事件
+  function addNextMon(date,type){
+    let nextMon = document.getElementById(type.nextMon);
+    nextMon.addEventListener("click", function(e){
+        if( date.month<12){
+           date.month++;
+        }else{
+          date.year++;
+          date.month = 1;
+        }
+       show(date,type);
+    })
+  }
+
+  //添加前一年点击事件
+  function addPreYear(date,type){
+    let preYear = document.getElementById(type.preYear);
+    preYear.addEventListener("click", function(e){
+      date.year--
+      show(date,type);
+    })
+  }
+
+  //添加前一年点击事件
+  function addNextYear(date,type){
+    let nextYear = document.getElementById(type.nextYear);
+    nextYear.addEventListener("click", function(e){
+        date.year++
+        show(date,type);
+    })
+  }
+
+  //添加选择日期事件
+  function addSelect(date,type){
+    let objTable = document.getElementById(type.objTable);
+    objTable.addEventListener("click",function(e){
+     if(e.target.nodeName === "TD" && e.target.innerHTML !==""){
+       date.day = Number(e.target.innerHTML);
+       currentShow(type,date.day)
+      }
+    })
+  }
+
+  function show(date,type){
+    let year = date.year;
+    let month = date.month;
+    let day = date.day;
+    let objMonth = document.getElementById(type.objTitle);
+    objMonth.innerHTML =year + "年" + month + "月";
+    type.curTrArr = creatDate(date);
+    currentShow(type,day);
+  }
+
+
+  function currentShow(type,day){
+     let html = renderDate(type.curTrArr,day,type);
+     let objTable = document.getElementById(type.objTable);
+     objTable.innerHTML = html;
+  }
+
+  // ********************纯函数************************
+
+  //创建日期的二维数组;
+  function creatDate(date){
+    let year = date.year;
+    let month = date.month;
+    let day = date.day;
+    let str = year + "-"+ month + "-" + "1";
+    let week = new Date(str).getDay();
+    let dayCountObj = new Date(year,month,0);
+    let dayCount = dayCountObj.getDate();
+    let j = 1;
+    let trArr = [];
+    for (let a = 0; a < 6; a++){
+      trArr[a] = [];
+      for (let i = 0; i < 7; i++){
+        if( (i >= week || a > 0) && j<= dayCount){
+             trArr[a][i] = j++;
+         }else {
+             trArr[a][i] = "";
+        }
+      }
+    } 
+    return trArr;
+  }
+
+  //渲染成HTML模版
+  function renderDate(trArr,day,type){
+    let html = "<tr><th>日</th><th>一</th><th>二</th><th>三</th><th>四</th><th>五</th><th>六</th></tr>";
+    for (let i = 0; i < 6; i++){
+     if(i<5||(i == 5 && trArr[i][0]!=='')){
+        let template = "";
+        for(let n = 0; n < 7; n++){
+           if(trArr[i][n] == day){
+             template += "<td  class='date-select'>"+trArr[i][n]+"</td>"
+           }else {
+             template += "<td>"+trArr[i][n]+"</td>"
+           }
+        }
+        templateBox = "<tr>"+template+"</tr>";
+        html += templateBox;
+     } 
+    }
+    return html;
+  }
+}
+
+newDate.prototype.show = function(){
+  let obj = document.getElementById('date-components');
+  obj.className = 'date-components  date-components-show'
+}
+
+// //闭包拿到两个date数据
+// let getTwoDate = newDate.getDate()
+
+newDate.prototype.addConfirmEvent = function(fn){
+   let obj = document.getElementById('date-components');
+   confirmDate.addEventListener("click", function(){
+      var confirmDate = document.getElementById("confirmDate");
+      obj.className = 'date-components';
+      fn();
+   })
 }
--- util/test.js
+++
@@ -1,9 +0,0 @@
-export const sqrt = Math.sqrt;
-//导出函数
-export function square(x) {
-    return x * x;
-}
-//导出函数
-export function diag(x, y) {
-    return sqrt(square(x) + square(y));
-}
